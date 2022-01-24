61d7ac2a9549855ad757405146242351b384c051
Vishal Mandora <v_mandora@yahoo.com>
2022-01-24 11:21:03 +0530

    Missing packages added & configured

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::prefix('secret')->group(function () {
+    Route::get('logs', [\Rap2hpoutre\LaravelLogViewer\LogViewerController::class, 'index']);
+});
